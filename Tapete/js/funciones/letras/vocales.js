b2ddca71fbc92a71f7359b1d777b1eb3248c1724
24majo <marijoguerrero24@gmail.com>
2024-05-09 22:23:54 -0600

    vocales

--- Tapete/js/funciones/letras/vocales.js
+++ Tapete/js/funciones/letras/vocales.js
@@ -1,4 +1,4 @@
-const palabras = ["Tortuga", "Gato", "Perro", "Pollo"]
+const palabras = ["dulces", "helado", "perro", "pollo", "gato", "manzana"]
 const vocales = ["a", "e", "i", "o", "u"]
 var opciones = document.getElementsByClassName("opcion")
 var contador = 0
@@ -13,9 +13,8 @@ function Inicio(){
     var guion = respuesta.replace(/a|e|i|o|u/g, "_") 
     objetos.guion = guion
     document.getElementById("linea").innerHTML = objetos.guion
+     // Mostrar la palabra incompleta
     opcion(arr_op = [], 0, respuesta)
-
-    document.getElementById('circulos').style.display='block';
 }
 
 function Reinicio(){
@@ -28,9 +27,12 @@ function opcion(arr_op, aux, respuesta){ // Función recursiva
         for (let i = 0; i <= respuesta.length; i++){
             var a = respuesta.indexOf(vocales[i])
             if(a != -1){ // Cuando es diferente de -1 significa que si existe
+                //vocal_a.push(vocales[i])
                 console.log("Vocal: ",vocales[i])
+                //console.log("i: ", i)
                 var e = arr_op.includes(vocales[i]) // Se evalúa si la vocal existente está entre las opciones
                 if(!e){ // Si no existe, se elige una posición aleatoria de las opciones y se agrega
+                    //console.log("Antes: ", arr_op)
                     let r = Math.floor(Math.random() * arr_op.length)
                     arr_op.splice(r, 1, vocales[i])
                     //console.log("Después: ", arr_op)
@@ -63,7 +65,9 @@ const replaceAt = (string, character, index) => {
 function validar(valor){
     for(let i = 0; i < objetos.resp.length; i++){
         if(objetos.resp[i] == objetos.opc_arr[valor]){
+            //console.log("letra seleccionada: ",objetos.opc_arr[valor])
             objetos.guion = replaceAt(objetos.guion, objetos.opc_arr[valor], i)
+            //console.log("si")
         }
     }
     document.getElementById("linea").innerHTML = objetos.guion
@@ -73,6 +77,7 @@ function validar(valor){
         contador++
         document.getElementById("contador").innerHTML = contador
         console.log(contador)
+        //Inicio()
     }
 }
 
@@ -122,5 +127,71 @@ window.addEventListener("keyup",(e)=>{
     }
 })
 
-var imagen = document.getElementById("img")
-imagen.src = "Visual/Material/Letras/Juego1/Animales/" + respuesta + ".png"
+/*var respuesta = "" // Se guarda una de las palabras que están en el arreglo
+var opciones = document.getElementsByClassName("opcion")
+var apoyo = [] // Arreglo de apoyo para letras en circulos
+
+
+
+window.addEventListener("keyup",(e)=>{
+    let tecla = e.key
+
+        switch(tecla){
+        case 'ArrowUp':
+            break;
+        case 'ArrowDown':
+            break;
+        case 'ArrowLeft':
+            break;
+        case 'ArrowRight':
+            break;
+        default:
+            break;
+        }
+    })
+
+function palabra(){
+    var r = Math.floor(Math.random() * palabras.length) // Elegir un numero aleatorio 
+    respuesta = palabras[r]
+    document.getElementById("palabra").innerHTML = respuesta // Mostrar la palabra en pantalla
+    var identificar = respuesta.split(''); // Separar cada letra de la palabra
+    
+    let vocal_aux = ["","","",""] // Auxiliar para guardar las vocales que existen en la palabra
+    let n = 0
+    var lineas = [] // Arreglo para dibujar las lineas y completar la palabra
+    
+    for(var i = 0; i < identificar.length; i++){
+        lineas[i] = identificar[i] // Aumentar el tamaño del arreglo de acuerdo a la longitud de la palabra aleatoria
+        for(var j = 0; j < vocales.length; j++){
+            if(identificar[i] == vocales[j]){ // Identificar las vocales que existen en una palabra
+                lineas[i] = "_" // Si encuentra vocales, las reemplaza por un guion
+                vocal_aux[n] = identificar[i]
+                n++
+            }
+        }
+    }
+    document.getElementById("linea").innerHTML = lineas // Mostrar lineas en pantalla
+    console.log("Antes: ", vocal_aux)
+    //return vocal_aux
+}*/
+    
+
+/*    for(var i = vocal_aux.length -1; i >=0; i--){ // Eliminar elementos repetidos
+        if(vocal_aux.indexOf(vocal_aux[i]) !== i) 
+            vocal_aux.splice(i,1);
+    }
+
+    var op = ["", "", "", ""]
+    for(var i = 0; i < op.length; i++){
+        let r = Math.floor(Math.random() * vocales.length)
+        op[i] = vocales[r]
+    }
+
+    for(var i = op.length -1; i >=0; i--){
+        if(op.indexOf(op[i]) !== i)
+            op.splice(i,1)
+        opciones[i].innerHTML = op[i]
+    }
+
+    console.log(vocal_aux)
+    console.log(op)*/
