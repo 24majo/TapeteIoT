99776bd893cd5723413f80254acbc81bc6b5195a
24majo <marijoguerrero24@gmail.com>
2024-05-10 20:55:31 -0600

    adivinar

---
+++ Tapete/js/funciones/letras/adivinar.js
@@ -0,0 +1,9 @@
+const palabras = ["Tortuga", "Perro", "Pollo", "Gato"]
+var opciones = document.getElementsByClassName("opcion")
+const objetos ={
+    guion: undefined,
+    respuesta: undefined
+}
+function Inicio(){
+
+}
