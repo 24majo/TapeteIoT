92a41f13f40c387966a75bd129240a57f1b1552f
24majo <marijoguerrero24@gmail.com>
2024-05-10 21:43:27 -0600

    prueba

--- Tapete/js/funciones/letras/adivinar.js
+++ Tapete/js/funciones/letras/adivinar.js
@@ -7,3 +7,41 @@ const objetos ={
 function Inicio(){
 
 }
+
+window.addEventListener("keydown",(e)=>{
+    let tecla = e.key
+
+    switch(tecla){
+    case 'ArrowUp':
+        break;
+    case 'ArrowDown':
+        break;
+    case 'ArrowLeft':
+        break;
+    case 'ArrowRight':
+        break;
+    default:
+        break;
+    }
+})
+
+window.addEventListener("keyup",(e)=>{
+    let tecla = e.key
+
+    switch(tecla){
+        case 'ArrowUp':
+            break;
+
+        case 'ArrowDown':
+            break;
+
+        case 'ArrowLeft':
+            break;
+
+        case 'ArrowRight':
+            break;
+
+        default:
+            break;
+    }
+})
