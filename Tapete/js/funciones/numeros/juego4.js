e040a5e344f92e1b4ac1373c25b72a056b02929e
24majo <marijoguerrero24@gmail.com>
2024-08-07 20:28:31 -0600

    juego4_estructura

---
+++ Tapete/js/funciones/numeros/juego4.js
@@ -0,0 +1,17 @@
+// Vidas
+var imagen = document.getElementById('vida');
+imagen.innerHTML = '<img src="Visual/Material/Numeros/Juego1/3.jpg" width="100">'
+var error = 3
+// Barra de progreso
+contador = 0
+document.getElementById("barra").value = contador
+document.getElementById("barra").innerHTML = contador
+// Circulos de opciones
+var opciones = document.getElementsByClassName("opcion")
+var op = []
+//Opciones de dificultad
+var radios = document.getElementsByName("dificultad")
+// Elementos generales
+var resultado = 0
+var arreglo = []
+
