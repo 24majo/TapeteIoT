7612c6d9153f4ac470e1d227a6c9fb72adac6226
24majo <marijoguerrero24@gmail.com>
2024-08-07 22:59:50 -0600

    juego4_funciones

--- Tapete/js/funciones/numeros/juego4.js
+++ Tapete/js/funciones/numeros/juego4.js
@@ -15,3 +15,90 @@ var radios = document.getElementsByName("dificultad")
 var resultado = 0
 var arreglo = []
 
+function Empezar(){
+    if(!document.querySelector('input[name="dificultad"]:checked')){
+        alert("Selecciona algo ptm")
+    }
+
+    else{
+        // for(let y = 0; y < 3; y++){
+        //     radios[y].disabled = true
+        // }
+        valor = document.querySelector('input[name="dificultad"]:checked').value
+        document.getElementById('aparecer').style.display='block';
+
+        switch(valor){
+            case 'facil':
+                num1 = Math.floor(Math.random() * (50-1)+1)
+                num2 = Math.floor(Math.random() * (num1-1)+1)
+                Random(num1, num2)  
+                break
+
+            case 'medio':
+                num1 = Math.floor(Math.random() * (100-50)+50)
+                num2 = Math.floor(Math.random() * (num1-50)+50)
+                Random(num1, num2)  
+                break
+            
+            case 'dificil':
+                num1 = Math.floor(Math.random() * (100-1)+1)
+                num2 = Math.floor(Math.random() * (num1-1)+1)
+                //alert("Num1: " + num1 + " num2: " + num2)
+                Random(num1, num2) 
+                break
+        }
+    }
+}
+
+function Reinicio(){
+
+}
+
+// Eventos del teclado compatibles con botones
+window.addEventListener("keydown",(e)=>{
+    let tecla = e.key
+    switch(tecla){
+        case 'ArrowUp':
+            RCorrecto(op[0])
+            break;
+
+        case 'ArrowDown':
+            RCorrecto(op[1])
+            break;
+
+        case 'ArrowLeft':
+            RCorrecto(op[2])
+            break;
+
+        case 'ArrowRight':
+            RCorrecto(op[3])
+            break;
+
+        default:
+            break;
+    }
+})
+
+window.addEventListener("keyup",(e)=>{
+    let tecla = e.key
+    switch(tecla){
+        case 'ArrowUp':
+            Random(0)
+            break;
+
+        case 'ArrowDown':
+            Random(1)
+            break;
+
+        case 'ArrowLeft':
+            Random(2)
+            break;
+
+        case 'ArrowRight':
+            Random(3)
+            break;
+
+        default:
+            break;
+    }
+})
