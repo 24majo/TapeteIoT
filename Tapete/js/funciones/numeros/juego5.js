d6722324d1605ea3be6f69d0430877b2165fcfb3
24majo <157389928+24majo@users.noreply.github.com>
2024-08-25 01:37:15 -0600

    juego5_errores

--- Tapete/js/funciones/numeros/juego5.js
+++ Tapete/js/funciones/numeros/juego5.js
@@ -77,13 +77,13 @@ function Empezar(){
                 break
 
             case 'medio':
-                num = Math.floor(Math.random() * (100-51)+51)
+                num = Math.floor(Math.random() * (99-51)+51)
                 pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="100">'
                 Completar(num)  
                 break
             
             case 'dificil':
-                num = Math.floor(Math.random() * (100-1)+1)
+                num = Math.floor(Math.random() * (99-1)+1)
                 pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="100">'
                 Completar(num) 
                 break
@@ -106,7 +106,7 @@ function Completar(num){
 
 function Opcion(arreglo){
     if(arreglo.length == 4){
-        //alert("res: " + respuesta)
+        alert("res: " + respuesta)
         var res = arreglo.indexOf(respuesta)
         if(res == -1){
             var r = Math.floor(Math.random() * arreglo.length) 
@@ -115,7 +115,7 @@ function Opcion(arreglo){
     }
 
     else{
-        var r = Math.floor(Math.random() * ((10-1)+1))
+        var r = Math.floor(Math.random() * ((9-1)+1))
         arreglo.push(r)
                 
         result = arreglo.filter((item,index)=>{
