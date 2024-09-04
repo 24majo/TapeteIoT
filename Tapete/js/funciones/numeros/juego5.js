dfa8d1342dc50cfa42dc04493efaa944ce473a49
Lucero-Albor <lucerito856@gmail.com>
2024-09-04 13:07:33 -0600

    Acomodo interfaces terminadas

--- Tapete/js/funciones/numeros/juego5.js
+++ Tapete/js/funciones/numeros/juego5.js
@@ -71,7 +71,7 @@ function Empezar(){
             case 'facil':
                 num = Math.floor(Math.random() * (50-1)+1)
                 decena = num - (num % 10) + 10
-                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="100">'
+                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="400">'
                 //alert("num: " + num)
                 Completar(num)  
                 break
@@ -79,14 +79,14 @@ function Empezar(){
             case 'medio':
                 num = Math.floor(Math.random() * (99-51)+51)
                 decena = num - (num % 10) + 10
-                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="100">'
+                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="400">'
                 Completar(num)  
                 break
             
             case 'dificil':
                 num = Math.floor(Math.random() * (99-1)+1)
                 decena = num - (num % 10) + 10
-                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="100">'
+                pecera.innerHTML = '<img src="Visual/Material/Numeros/Juego5/'+ num +'.jpg" width="400">'
                 Completar(num) 
                 break
         }
