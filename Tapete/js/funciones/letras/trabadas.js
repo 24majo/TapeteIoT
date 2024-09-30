58c93c5735bd5a488ab9a082bf1068efd31c79ac
24majo <157389928+24majo@users.noreply.github.com>
2024-09-29 21:04:54 -0600

    trabadas y mas

---
+++ Tapete/js/funciones/letras/trabadas.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    valor = localStorage.getItem('valorBoton');
+    alert(valor)
+}
+
+if(valor == "b"){
+    document.getElementById("opcion1").innerHTML == 'Bla'
+    document.getElementById("opcion2").innerHTML == 'Bra'
+}
+// if(valor == 'c'){
+
+// }
+// if(valor == 'f'){
+
+// }
+// if(valor == 'g'){
+
+// }
+// if(valor == 'p'){
+
+// }
+// if(valor == 't'){
+
+// }
+
