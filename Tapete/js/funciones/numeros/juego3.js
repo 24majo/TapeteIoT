a6847ecaeaf2c0d71d397dda4d0bd3e643383674
Lucero-Albor <lucerito856@gmail.com>
2024-10-09 10:41:38 -0600

    Acomodo sumas y restas

--- Tapete/js/funciones/numeros/juego3.js
+++ Tapete/js/funciones/numeros/juego3.js
@@ -79,8 +79,25 @@ function Random(num1, num2){
     //num1 = Math.round(num1 / 10) * 10
     //num2 = Math.round(num2 / 10) * 10
     resultado = num1 + num2
-    document.getElementById("linea_s").innerHTML = num1
-    document.getElementById("linea_i").innerHTML = "+" + num2
+    if(num1<=9){
+        document.getElementById("linea_s").innerHTML = "0"+num1
+    }else{
+        document.getElementById("linea_s").innerHTML = num1
+    }if(num2<=9){
+        document.getElementById("linea_i").innerHTML = "0"+num2
+    }else{
+        document.getElementById("linea_i").innerHTML = "+" + num2
+    }if(num1<=9 && num2<=9){
+        document.getElementById("linea_s").innerHTML = "0"+num1
+        document.getElementById("linea_i").innerHTML = "+"+"0"+num2
+    }if(num1>9 && num2<=9){
+        document.getElementById("linea_s").innerHTML = num1
+        document.getElementById("linea_i").innerHTML = "+"+"0"+num2
+    }if(num2>9 && num1<=9){
+        document.getElementById("linea_s").innerHTML = "0"+num1
+        document.getElementById("linea_i").innerHTML = "+"+num2
+    }
+    
 
     op = Opcion(arreglo = [])
 
