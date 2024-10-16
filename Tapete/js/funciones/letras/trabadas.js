214e5dd8fa2c273f9ad21e8ff73cf971f42c80cf
24majo <157389928+24majo@users.noreply.github.com>
2024-10-16 12:22:49 -0600

    Trabadas ya no tan trabadas

--- Tapete/js/funciones/letras/trabadas.js
+++ Tapete/js/funciones/letras/trabadas.js
@@ -13,47 +13,86 @@ var encabezado = localStorage.getItem("valor") // Envío de valor de acuerdo con
 var palabras = []
 var opciones = document.getElementsByClassName("opcion")
 var op = []
+var respuesta = ""
+var imagen = document.getElementById('imagen') 
+var tabla1 = document.getElementsByClassName("tabla1")
+var tabla2 = document.getElementsByClassName("tabla2")
+var t1 = 0, t2 = 0
+
 //alert(encabezado)
 
-if(encabezado == "b"){
-    document.getElementById("opcion1").textContent = "Bl"
-    document.getElementById("opcion2").textContent = "Br"
-    palabras = ["hablar", "nublado", "mueble", "oblea", "ombligo", "biblioteca", "pueblo", "establo", "bloque", "blusa", 
-        "sombra", "cebra", "sombrero", "libreta", "lombriz", "colibrí", "libro", "hombro", "bruja", "brújula"]
-    op = ["bl", "br"]
-    OpcionesCirculos(op)
-}
-if(encabezado == "c"){
-    document.getElementById("opcion1").textContent = "Cl"
-    document.getElementById("opcion2").textContent = "Cr"
-    op = ["cl", "cr"]
-    OpcionesCirculos(op)
-}
-if(encabezado == 'f'){
-    document.getElementById("opcion1").textContent = "Fl"
-    document.getElementById("opcion2").textContent = "Fr"
-    op = ["fl", "fr"]
-    OpcionesCirculos(op)
-}
-if(encabezado == 'g'){
-    document.getElementById("opcion1").textContent = "Gl"
-    document.getElementById("opcion2").textContent = "Gr"
-    op = ["gl", "gr"]
-    OpcionesCirculos(op)
-}
-if(encabezado == 'p'){
-    document.getElementById("opcion1").textContent = "Pl"
-    document.getElementById("opcion2").textContent = "Pr"
-    palabras = ["planta", "planeta", "empleo", "cumpleaños", "suplicar", "soplido", "explorador", "templo", "pluma", "plutón", 
-        "compra", "temprano", "precio", "aprender", "príncipe", "exprimir", "profesor", "producto", "prueba", "compruebo"]
-    op = ["pl", "pr"]
+function ValoresTabla(){
+    if(encabezado == "b"){
+        document.getElementById("opcion1").textContent = "Bl"
+        document.getElementById("opcion2").textContent = "Br"
+        palabras = ["hablar", "nublado", "mueble", "oblea", "ombligo", "biblioteca", "pueblo", "establo", "bloque", "blusa", 
+            "sombra", "cebra", "sombrero", "libreta", "lombriz", "colibrí", "libro", "hombro", "bruja", "brújula"]
+        //palabras = ["libro"]
+        op = ["bl", "br"]
         OpcionesCirculos(op)
+    }
+    if(encabezado == "c"){
+        document.getElementById("opcion1").textContent = "Cl"
+        document.getElementById("opcion2").textContent = "Cr"
+        op = ["cl", "cr"]
+        OpcionesCirculos(op)
+    }
+    if(encabezado == 'f'){
+        document.getElementById("opcion1").textContent = "Fl"
+        document.getElementById("opcion2").textContent = "Fr"
+        op = ["fl", "fr"]
+        OpcionesCirculos(op)
+    }
+    if(encabezado == 'g'){
+        document.getElementById("opcion1").textContent = "Gl"
+        document.getElementById("opcion2").textContent = "Gr"
+        op = ["gl", "gr"]
+        OpcionesCirculos(op)
+    }
+    if(encabezado == 'p'){
+        document.getElementById("opcion1").textContent = "Pl"
+        document.getElementById("opcion2").textContent = "Pr"
+        palabras = ["planta", "planeta", "empleo", "cumpleaños", "suplicar", "soplido", "explorador", "templo", "pluma", "plutón", 
+            "compra", "temprano", "precio", "aprender", "príncipe", "exprimir", "profesor", "producto", "prueba", "compruebo"]
+        op = ["pl", "pr"]
+            OpcionesCirculos(op)
+    }
+    if(encabezado == 't'){
+        document.getElementById("opcion1").textContent = "Tl"
+        document.getElementById("opcion2").textContent = "Tr"
+        op = ["tl", "tr"]
+        OpcionesCirculos(op)
+    }
 }
-if(encabezado == 't'){
-    document.getElementById("opcion1").textContent = "Tl"
-    document.getElementById("opcion2").textContent = "Tr"
-    op = ["tl", "tr"]
-    OpcionesCirculos(op)
+
+function Empezar(){
+    respuesta = palabras[Math.floor(Math.random() * palabras.length)]
+    var repetida = palabras.indexOf(respuesta)
+    palabras.splice(repetida, 1)
+    //alert(respuesta)
+    imagen.src = "Visual/Material/Letras/Juego8/" + respuesta + ".png"
+    document.getElementById("prueba").innerHTML = respuesta
+    Evaluar()
+}
+
+function Comprobar(arreglo){
+    if(respuesta.indexOf(arreglo) > -1){
+        if(arreglo == 'bl'){
+            tabla1[t1].src = "Visual/Material/Letras/Juego8/" + respuesta + ".png"
+            t1++
+        }
+
+        if(arreglo == 'br'){
+            tabla2[t2].src = "Visual/Material/Letras/Juego8/" + respuesta + ".png"
+            t2++
+        }
+        
+        Felicidades()
+    }
+
+    else{
+        Fallo()
+    }
 }
 
 function OpcionesCirculos(op){
@@ -66,19 +105,127 @@ window.addEventListener("keydown",(e)=>{
     let tecla = e.key
     switch(tecla){
     case 'ArrowLeft':
-        //alert(arreglo[0])
-            Comprobar(arreglo[0])
+        Comprobar(op[0])
         break;
     case 'ArrowRight':
-        //alert(arreglo[1])
-            Comprobar(arreglo[1])
+        Comprobar(op[1])
         break;
     default:
         break;
     }
 })
 
+function Reinicio(){
+    swal({
+        title: "Reiniciar juego",
+        text: "Si reinicias ahora, el progreso se perderá. ¿Deseas continuar?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+
+      .then((willDelete) => {
+        if (willDelete) {
+            document.getElementById("btnIniciar").innerHTML = "Empezar"
+            Reiniciar()
+        } 
+    });
+}
+
+function Reiniciar(){
+    error = 3
+    vida.innerHTML = '<img src="Visual/Material/Iconos/corazon3.png" width="100">'
+    contador = 0
+    palabras = []
+    respuesta = ""
+    document.getElementById("linea").innerHTML = ""
+    document.getElementById("barra").value = contador
+    document.getElementById("barra").innerHTML = contador
+    num_ejercicio = 0
+    num_opcion = 0
+
+    for(var i = 0; i < tabla1.length; i++){
+        tabla1[i].src = ""
+    }
+
+    for(var i = 0; i < tabla2.length; i++){
+        tabla2[i].src = ""
+    }
+
+    ValoresTabla()
+    Empezar()
+}
+
+function Fallo(){
+    error-- 
+    if(error == 2){
+        vida.innerHTML = '<img src="Visual/Material/Iconos/corazon2.png" width="100">'
+    }
+
+    if(error == 1){
+        vida.innerHTML = '<img src="Visual/Material/Iconos/corazon1.png" width="100">'
+    }
+
+    if(error == 0){
+        vida.innerHTML = ""
+        swal({
+            title: "¡Oh no!",
+            text: "No te quedan más vidas. ¿Deseas salir o reintentar?",
+            icon: "error",
+            buttons:  ["Reintentar", "Salir"] 
+        })
+        .then((reintento) => {
+            if (reintento) {
+                location.href = "JuegosLetras.html"
+            } 
+            else{
+                document.getElementById("btnIniciar").innerHTML = "Empezar"
+                Reiniciar()
+            }
+        })
+    }
+}
+
+function Felicidades(){
+    swal({
+        title: "¡Muy bien!",
+        text: "Continuemos. Sigue así",
+        icon: "Visual/Material/Animaciones/Generales/echeleganas.png"
+    })
+
+    .then((continuacion) => {
+        if (continuacion) {
+            document.getElementById("btnIniciar").innerHTML = "Continuar"
+            contador++
+            document.getElementById("barra").value = contador
+            document.getElementById("barra").innerHTML = contador
+
+            if(contador == 20){
+                swal({
+                    title: "Felicidades",
+                    text: "¿Quieres salir del juego o volver a intentarlo?",
+                    icon: "Visual/Material/Animaciones/Generales/pollo.gif",
+                    buttons:  ["Volver a jugar", "Salir"] 
+                })
+                .then((reintento) => {
+                    if (reintento) {
+                        location.href = "JuegosLetras.html"
+                    } 
+                    else{
+                        document.getElementById("btnIniciar").innerHTML = "Empezar"
+                        Reiniciar()
+                    }
+                })
+            }
+            else{
+                Empezar()
+            }
+        } 
+    })
+}
+
 //Ayuda()
+ValoresTabla()
 
 function Ayuda(){
     swal({
