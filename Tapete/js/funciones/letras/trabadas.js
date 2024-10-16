a17cd3132a2d093b36644002a123336f53aaf9f7
24majo <157389928+24majo@users.noreply.github.com>
2024-10-16 11:28:42 -0600

    más trabada estoy yo pero voy avanzando

--- Tapete/js/funciones/letras/trabadas.js
+++ Tapete/js/funciones/letras/trabadas.js
@@ -1,30 +1,84 @@
-var encabezado1 = localStorage.getItem("opcion1")
-document.getElementById("ayuda").onclick = Ayuda;
+// Vidas
+var error = 3
+var vida = document.getElementById('vida');
+vida.innerHTML = '<img src="Visual/Material/Iconos/corazon3.png" width="100">'
 
-if(encabezado1 == "b"){
-    document.getElementById("opcion1").textContent = "Bla"
-    document.getElementById("opcion2").textContent = "Bra"
+// Barra de progreso
+contador = 0
+document.getElementById("barra").value = contador
+document.getElementById("barra").innerHTML = contador
+
+// Elementos generales
+var encabezado = localStorage.getItem("valor") // Envío de valor de acuerdo con el modo de juego
+var palabras = []
+var opciones = document.getElementsByClassName("opcion")
+var op = []
+//alert(encabezado)
+
+if(encabezado == "b"){
+    document.getElementById("opcion1").textContent = "Bl"
+    document.getElementById("opcion2").textContent = "Br"
+    palabras = ["hablar", "nublado", "mueble", "oblea", "ombligo", "biblioteca", "pueblo", "establo", "bloque", "blusa", 
+        "sombra", "cebra", "sombrero", "libreta", "lombriz", "colibrí", "libro", "hombro", "bruja", "brújula"]
+    op = ["bl", "br"]
+    OpcionesCirculos(op)
 }
-if(encabezado1 == "c"){
-    document.getElementById("opcion1").textContent = "Cla"
-    document.getElementById("opcion2").textContent = "Cra"
+if(encabezado == "c"){
+    document.getElementById("opcion1").textContent = "Cl"
+    document.getElementById("opcion2").textContent = "Cr"
+    op = ["cl", "cr"]
+    OpcionesCirculos(op)
 }
-if(encabezado1 == 'f'){
-    document.getElementById("opcion1").textContent = "Fla"
-    document.getElementById("opcion2").textContent = "Fra"
+if(encabezado == 'f'){
+    document.getElementById("opcion1").textContent = "Fl"
+    document.getElementById("opcion2").textContent = "Fr"
+    op = ["fl", "fr"]
+    OpcionesCirculos(op)
 }
-if(encabezado1 == 'g'){
-    document.getElementById("opcion1").textContent = "Gla"
-    document.getElementById("opcion2").textContent = "Gra"
+if(encabezado == 'g'){
+    document.getElementById("opcion1").textContent = "Gl"
+    document.getElementById("opcion2").textContent = "Gr"
+    op = ["gl", "gr"]
+    OpcionesCirculos(op)
 }
-if(encabezado1 == 'p'){
-    document.getElementById("opcion1").textContent = "Pla"
-    document.getElementById("opcion2").textContent = "Pra"
+if(encabezado == 'p'){
+    document.getElementById("opcion1").textContent = "Pl"
+    document.getElementById("opcion2").textContent = "Pr"
+    palabras = ["planta", "planeta", "empleo", "cumpleaños", "suplicar", "soplido", "explorador", "templo", "pluma", "plutón", 
+        "compra", "temprano", "precio", "aprender", "príncipe", "exprimir", "profesor", "producto", "prueba", "compruebo"]
+    op = ["pl", "pr"]
+        OpcionesCirculos(op)
 }
-if(encabezado1 == 't'){
-    document.getElementById("opcion1").textContent = "Tla"
-    document.getElementById("opcion2").textContent = "Tra"
+if(encabezado == 't'){
+    document.getElementById("opcion1").textContent = "Tl"
+    document.getElementById("opcion2").textContent = "Tr"
+    op = ["tl", "tr"]
+    OpcionesCirculos(op)
 }
+
+function OpcionesCirculos(op){
+    for(var i = 0; i < opciones.length; i++){
+        opciones[i].innerHTML = op[i]
+    }
+}
+
+window.addEventListener("keydown",(e)=>{
+    let tecla = e.key
+    switch(tecla){
+    case 'ArrowLeft':
+        //alert(arreglo[0])
+            Comprobar(arreglo[0])
+        break;
+    case 'ArrowRight':
+        //alert(arreglo[1])
+            Comprobar(arreglo[1])
+        break;
+    default:
+        break;
+    }
+})
+
+//Ayuda()
 
 function Ayuda(){
     swal({
