cdfa5a3bcef1cdc5a15ce06be1c00a179ebff621
24majo <157389928+24majo@users.noreply.github.com>
2024-11-03 22:02:20 -0600

    formato correo

--- Tapete/conexiones/registro_admin.php
+++ Tapete/conexiones/registro_admin.php
@@ -12,13 +12,13 @@
         $respuesta = $_POST['respuesta'];
         $rol = $_POST['rol'];
 
-        if($pass != $conf_pass){
+        if (!preg_match("/^[\w\.-]+@[\w\.-]+\.\w{2,}$/", $correo)) {
             echo '<script src="../node_modules/sweetalert/dist/sweetalert.min.js"></script>';
             echo 
             '<script>
                 swal({
                     title: "Error",
-                    text: "Comprueba que las contraseñas sean iguales",
+                    text: "Formato de correo no válido",
                     icon: "error"
                 })
 
@@ -31,39 +31,59 @@
         }
 
         else{
-            function Numero() {
-                $aleatorio = rand(0, 999);
-                $numero = str_pad($aleatorio, 3, '0', STR_PAD_LEFT);
-                return $numero;
+            if($pass != $conf_pass){
+                echo '<script src="../node_modules/sweetalert/dist/sweetalert.min.js"></script>';
+                echo 
+                '<script>
+                    swal({
+                        title: "Error",
+                        text: "Comprueba que las contraseñas sean iguales",
+                        icon: "error"
+                    })
+
+                    .then((Okay) => {
+                        if (Okay) {
+                            window.location.href = "../registro_admin.html";
+                        } 
+                    });
+                </script>';
             }
 
-            function generarID($numero, $nombre, $paterno, $materno) {
-                $letraN = substr($nombre, 0, 2);
-                $letraP = substr($paterno, 0, 2);
-                $letraM = substr($materno, 0, 2);
-            
-                $num_empleado = $numero . strtoupper($letraN . $letraP . $letraM);
-            
-                return $num_empleado;
-            }
+            else{
+                function Numero() {
+                    $aleatorio = rand(0, 999);
+                    $numero = str_pad($aleatorio, 3, '0', STR_PAD_LEFT);
+                    return $numero;
+                }
 
-            $numero = Numero();
-            $num_empleado = generarID($numero, $nombres, $paterno, $materno);
+                function generarID($numero, $nombre, $paterno, $materno) {
+                    $letraN = substr($nombre, 0, 2);
+                    $letraP = substr($paterno, 0, 2);
+                    $letraM = substr($materno, 0, 2);
+                
+                    $num_empleado = $numero . strtoupper($letraN . $letraP . $letraM);
+                
+                    return $num_empleado;
+                }
 
-            $pass_encryp = password_hash($pass, PASSWORD_DEFAULT);
-            $resp_encryp = password_hash($respuesta, PASSWORD_DEFAULT);
+                $numero = Numero();
+                $num_empleado = generarID($numero, $nombres, $paterno, $materno);
 
-            $sql = "INSERT INTO docentes (num_empleado, nombre, materno, paterno, correo, password, pregunta, respuesta, rol)
-            VALUES ('$num_empleado', '$nombres', '$paterno', '$materno', '$correo', '$pass_encryp', '$pregunta', '$resp_encryp','$rol')";
+                $pass_encryp = password_hash($pass, PASSWORD_DEFAULT);
+                $resp_encryp = password_hash($respuesta, PASSWORD_DEFAULT);
 
-            $result = mysqli_query($conn, $sql);
-            if ($result) {
-                echo "Usuario registrado correctamente";
-                header("Location: ../registro_admin.html");
-            } 
-            
-            else {
-                echo "Error al registrar el usuario";
+                $sql = "INSERT INTO docentes (num_empleado, nombre, materno, paterno, correo, password, pregunta, respuesta, rol)
+                VALUES ('$num_empleado', '$nombres', '$paterno', '$materno', '$correo', '$pass_encryp', '$pregunta', '$resp_encryp','$rol')";
+
+                $result = mysqli_query($conn, $sql);
+                if ($result) {
+                    echo "Usuario registrado correctamente";
+                    header("Location: ../registro_admin.html");
+                } 
+                
+                else {
+                    echo "Error al registrar el usuario";
+                }
             }
         }
     }
