227f3958e42d206fabc69382c2cbcf35953b8d93
24majo <157389928+24majo@users.noreply.github.com>
2024-11-21 23:08:15 -0600

    sumas jalando ando

--- Tapete/js/funciones/numeros/juego3.js
+++ Tapete/js/funciones/numeros/juego3.js
@@ -1,8 +1,8 @@
-// Vidas
 var imagen = document.getElementById('vida');
 imagen.innerHTML = '<img src="Visual/Material/Iconos/corazon3.png" width="100">'
 var incorrecto = 3
 // Barra de progreso
+var contador2 = 0
 contador = 0
 document.getElementById("barra").value = contador
 document.getElementById("barra").innerHTML = contador
@@ -15,12 +15,30 @@ var valorBoton
 var resultado = 0
 var arreglo = []
 var semaforo = document.getElementById('semaforo')
+var puntaje = 10
 
 Ayuda() // Tutorial al abrir la pestaña por primera vez
 
+function Progreso(progreso,puntaje){
+    $.ajax({
+        url: 'conexiones/actualizar_progreso_a.php',  
+        type: 'POST',
+        data: {
+            progreso: progreso, 
+            puntaje: puntaje,
+            num_juego: 3,
+        },
+        success: function(response) {
+            console.log('Progreso actualizado', response);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error al actualizar el progreso: ' + error);
+        }
+    });
+}
+
 window.onload = function() {
     valor = localStorage.getItem('valorBoton');
-    //alert(valor)
 
     if(valor == 'facil')
         semaforo.src = "Visual/Material/Recursos/SemaforoFacil.png"
@@ -32,21 +50,7 @@ window.onload = function() {
         semaforo.src = "Visual/Material/Recursos/SemaforoDificil.png"
 }
 
-
-
 function Empezar(){
-    // if(!document.querySelector('input[name="dificultad"]:checked')){
-    //     swal({
-    //         title: "Advertencia",
-    //         text: "Elige una dificultad para iniciar el juego",
-    //         icon: "warning", 
-    //     })
-    // }
-    //else{
-        // for(let y = 0; y < 3; y++){
-        //     radios[y].disabled = true
-        // }
-        // valor = document.querySelector('input[name="dificultad"]:checked').value
     document.getElementById('aparecer').style.display='block';
     
     switch(valor){
@@ -54,14 +58,11 @@ function Empezar(){
             num1 = Math.floor(Math.random() * (50-1)+1)
             num2 = Math.floor(Math.random() * (50-1)+1)
             Random(num1, num2)  
-            // imagen.innerHTML = '<img src="Visual/Material/Recursos/SemaforoFacil.png" width="100">'
-            // document.getElementById("dificultad").textContent = "Fácil";
             break
 
         case 'medio':
             num1 = Math.floor(Math.random() * (100-50)+50)
             num2 = Math.floor(Math.random() * (100-50)+50)
-            //alert("Num1: " + num1 + " num2: " + num2)
             Random(num1, num2)  
             break
         
@@ -71,12 +72,9 @@ function Empezar(){
             Random(num1, num2) 
             break
     }
-    //}
 }
 
 function Random(num1, num2){
-    //num1 = Math.round(num1 / 10) * 10
-    //num2 = Math.round(num2 / 10) * 10
     resultado = num1 + num2
     if(num1<=9){
         document.getElementById("linea_s").innerHTML = "0"+num1
@@ -97,7 +95,6 @@ function Random(num1, num2){
         document.getElementById("linea_i").innerHTML = "+"+num2
     }
     
-
     op = Opcion(arreglo = [])
 
     for(let i = 0; i < opciones.length; i++){
@@ -141,13 +138,16 @@ function Racierto(num){
     if(num == resultado){
         PolloBueno()
         contador+=1
-        //alert("Vas acierto", contador)
+        contador2++
         document.getElementById("btnIniciar").innerHTML = "Continuar"
         document.getElementById("barra").value = contador
         document.getElementById("barra").innerHTML = contador
-
+        Progreso(contador2, puntaje)
+        
         if(contador == 10){
             if(valor == "dificil"){
+                contador2 = 10
+                Progreso(contador2, puntaje)
                 swal({
                     title: "¡Ganador!",
                     text: "Completaste todos los niveles. ¿Deseas salir o reiniciar?",
@@ -178,6 +178,8 @@ function Racierto(num){
                 .then((willDelete) => {
                     if (willDelete) {
                         if(valor == 'facil'){
+                            contador2 = 3.5
+                            Progreso(contador2, puntaje)
                             valor = "medio"
                             semaforo.src = "Visual/Material/Recursos/SemaforoMedio.png"
                             Reiniciar()
@@ -185,6 +187,8 @@ function Racierto(num){
 
                         else{
                             if(valor == 'medio'){
+                                contador2 = 7
+                                Progreso(contador2, puntaje)
                                 valor = "dificil"
                                 semaforo.src = "Visual/Material/Recursos/SemaforoDificil.png"
                                 Reiniciar()
@@ -193,38 +197,27 @@ function Racierto(num){
                     } 
                 })
             }
-
-            // var pregunta = window.confirm('¿Deseas continuar a la siguiente difilcutad?');
-            // if (pregunta === true) {
-            //     if(valor == 'facil'){
-            //         valor = document.querySelector('#medio').checked = true
-            //         Reiniciar()
-            //     }
-            //     else{
-            //         if(valor == 'medio'){
-            //             valor = document.querySelector('#dificil').checked = true
-            //             Reiniciar()
-            //         }
-            //         else{
-            //             alert("Ya ahora si ganaste todo, vete")
-            //         }
-            //     }
-            // }
         }
     }
     else{
         incorrecto--
         if(incorrecto == 2){
             imagen.innerHTML = '<img src="Visual/Material/Iconos/corazon2.png" width="100">'
+            puntaje -= 0.3
+            Progreso(contador2, puntaje)
             Polloincorrectoo()
         }
 
         if(incorrecto == 1){
             imagen.innerHTML = '<img src="Visual/Material/Iconos/corazon1.png" width="100">'
+            puntaje -= 0.3
+            Progreso(contador2, puntaje)
             Polloincorrectoo()
         }
 
         if(incorrecto == 0){
+            puntaje -= 0.3
+            Progreso(contador2, puntaje)
             Polloincorrectoo()
             imagen.innerHTML = ""
             swal({
@@ -251,13 +244,6 @@ function Racierto(num){
                         })
                         .then((cambiar) => {
                             if(cambiar){
-                                // for(let y = 0; y < 3; y++){
-                                //     radios[y].disabled = false
-                                // }
-                                // for (var i = 0; i < radios.length; i++) {
-                                //     var niveles = radios[i];
-                                //     niveles.checked = false;
-                                // }
 
                                 if(valor == 'dificil'){
                                     valor = "medio"
@@ -287,6 +273,8 @@ function Racierto(num){
 function Reiniciar(){
     incorrecto = 3
     contador = 0
+    puntaje = 10
+    contador2 = 0
     imagen.innerHTML = '<img src="Visual/Material/Iconos/corazon3.png" width="100">'
     document.getElementById("barra").value = contador
     document.getElementById("barra").innerHTML = contador
@@ -300,31 +288,14 @@ function Reinicio(){
         icon: "Visual/Material/Animaciones/Generales/advertencia.jpg",
         buttons: true,
         dangerMode: true,
-      })
+    })
 
-      .then((willDelete) => {
+    .then((willDelete) => {
         if (willDelete) {
-            // for(let y = 0; y < 3; y++){
-            //     radios[y].disabled = false
-            // }
-            // for (var i = 0; i < radios.length; i++) {
-            //     var niveles = radios[i];
-            //     niveles.checked = false;
-            // }
             document.getElementById("btnIniciar").innerHTML = "Empezar"
             Reiniciar()
         } 
     });
-    
-    // var pregunta = window.confirm('Al reiniciar, se perderá el progreso actual');
-    // if (pregunta === true) {
-    //     window.alert('Conste we');
-
-        
-    // } 
-    // else { 
-    //     window.alert('Se te hace así');
-    // }
 }
 
 window.addEventListener("keydown",(e)=>{
@@ -381,48 +352,44 @@ function Ayuda(){
         text: "Realiza la suma de dos números. Elige la opción correcta por medio de las teclas ↑ ↓ → ← o los botones del tablero.",
         icon: "Visual/Material/Animaciones/Generales/teclas.jpg"
     })
-    // $.dialog({
-    //     title: 'Tutorial',
-    //     content: 'Realiza la suma de dos números y elige la opción correcta con los botones o las flechas del teclado.',
-    // });
 }
 
 function PolloBueno(){
     const espera = document.getElementById("espera");
     const acierto = document.getElementById("acierto");
-  
+
     //Ocultar la animación de espera para pasar a la de acierto
     espera.classList.add("desaparecer");
-  
+
     //Muestra la aninmación de acierto una vez
     acierto.classList.remove("desaparecer");
     acierto.classList.add("acierto");
-  
+
     //Se usa el evento animationend para indicar que la animación finalizó 
     //y de nuevo muestre la animación de espera
     acierto.addEventListener("animationend", function() {
         //Ocultar la animación de acierto
-      acierto.classList.add("desaparecer");    
-      acierto.classList.remove("acierto");
+    acierto.classList.add("desaparecer");    
+    acierto.classList.remove("acierto");
         //Mostrar la animación de espera
-      espera.classList.remove("desaparecer"); 
-      //Ayuda a que la animación se ejecute una vez
+    espera.classList.remove("desaparecer"); 
+    //Ayuda a que la animación se ejecute una vez
     }, { once: true });
 }
 
 function Polloincorrectoo(){
     const espera = document.getElementById("espera");
     const incorrecto = document.getElementById("incorrecto");
-  
+
     espera.classList.add("desaparecer");
-  
+
     incorrecto.classList.remove("desaparecer");
     incorrecto.classList.add("incorrecto");
-  
+
     incorrecto.addEventListener("animationend", function() {
         incorrecto.classList.add("desaparecer"); 
         incorrecto.classList.remove("incorrecto");
-  
+
         espera.classList.remove("desaparecer"); 
     }, { once: true });
 }
