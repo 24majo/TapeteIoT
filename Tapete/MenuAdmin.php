ff5485a8b1045f70da5876894d7444d31a8061d9
24majo <157389928+24majo@users.noreply.github.com>
2024-12-01 00:10:54 -0600

    gráficas admin

--- Tapete/MenuAdmin.php
+++ Tapete/MenuAdmin.php
@@ -11,6 +11,43 @@ include'conexiones/conexion.php';
     <link rel="stylesheet" href="jquery/jquery-ui.structure.css">
     <link rel="stylesheet" href="jquery/jquery-ui.theme.css">
     <link rel="stylesheet" href="css/StyleMenuAdmin.css">
+
+    <style>
+    @keyframes rellenar{
+        to{
+            stroke-dasharray: var(--porcentaje) 100;
+        }
+    }
+
+    .porcentajes{
+        position:relative;
+    }
+    .porcentajes span{
+        position: absolute;
+        top: 0%;
+        left: 0%;
+        bottom: 0%;
+        right: 0%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font: 25px/1em Verdana;
+    }
+
+    circle{
+        fill: none;
+        stroke-width: 20;
+        transform: rotate(-90deg);
+        transform-origin: 50%;
+        stroke-dasharray: 100 100;
+        stroke: #AAA;
+    }
+    circle:nth-child(2){ /* el segundo círculo, es el que se ve por encima del anterior y debe tener el color mas intenso y el area del porcentaje */
+        stroke: var(--color);
+        stroke-dasharray: 0 100;
+        animation: rellenar .35s linear forwards;
+    }
+  </style>
 </head>
 <body>
     <button onclick="window.location.href = 'registro_admin.html'">Registrar personal</button>
@@ -60,17 +97,69 @@ include'conexiones/conexion.php';
             $general->execute();
             $pro_gen = $general->get_result();
             
-            // Impresión de los datos
             if ($pro_gen->num_rows > 0) {
                 while ($row = $pro_gen->fetch_assoc()) {
                     $p_general = round($row['promedio_g'], 1);
                     $p_numeros = round($row['promedio_n'], 1);
                     $p_letras = round($row['promedio_l'], 1);
 
-                    echo "<span><strong>Grupo " . $row['grupo'] . "</strong></span><br>
-                    <span><strong>Promedio general: </strong>" . $p_general . "</span><br>
-                    <span><strong>Promedio números: </strong>" . $p_numeros . "</span><br>
-                    <span><strong>Promedio letras: </strong>" . $p_letras . "</span><br><br>";
+                    if ($p_general < 6) {
+                        $color = 'red';
+                    } elseif ($p_general >= 6.1 && $p_general <= 8) {
+                        $color = 'green'; 
+                    } else {
+                        $color = 'blue';
+                    }
+
+                    // echo "<span><strong>Grupo " . $row['grupo'] . "</strong></span><br>
+                    // <span><strong>Promedio general: </strong>" . $p_general . "</span><br>
+                    // <span><strong>Promedio números: </strong>" . $p_numeros . "</span><br>
+                    // <span><strong>Promedio letras: </strong>" . $p_letras . "</span><br><br>";
+
+                    echo "<span><strong>Grupo " . $row['grupo'] . "</strong></span><br>";
+
+                    echo
+                    '<span><strong>Promedio general: </strong>'. $p_general . '</span><br>
+                    <div class="porcentajes" style="--porcentaje: '.number_format($p_general, 1).'; --color: '.$color.'">
+                        <svg width="150" height="150">
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                        </svg>
+                    </div>';
+
+                    if ($p_numeros < 6) {
+                        $color = 'red';
+                    } elseif ($p_numeros >= 6.1 && $p_numeros <= 8) {
+                        $color = 'green'; 
+                    } else {
+                        $color = 'blue';
+                    }
+
+                    echo
+                    '<span><strong>Promedio números: </strong>'. $p_numeros .'</span><br>
+                    <div class="porcentajes" style="--porcentaje: '.number_format($p_numeros, 1).'; --color: '.$color.'">
+                        <svg width="150" height="150">
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                        </svg>
+                    </div>';
+
+                    if ($p_letras < 6) {
+                        $color = 'red';
+                    } elseif ($p_letras >= 6.1 && $p_letras <= 8) {
+                        $color = 'green'; 
+                    } else {
+                        $color = 'blue';
+                    }
+
+                    echo
+                    '<span><strong>Promedio letras: </strong> '. $p_letras .' </span><br><br>
+                    <div class="porcentajes" style="--porcentaje: '.number_format($p_letras, 1).'; --color: '.$color.'">
+                        <svg width="150" height="150">
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                        </svg>
+                    </div>';
                 }
             } 
             else {
