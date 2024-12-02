9cbf70d3b8337a01a6ab068a603a5d395d52de25
Lucero-Albor <lucerito856@gmail.com>
2024-12-01 20:13:46 -0600

    Menu admin diseño finalizado :D

--- Tapete/MenuAdmin.php
+++ Tapete/MenuAdmin.php
@@ -5,77 +5,54 @@ include'conexiones/conexion.php';
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador</title>
-    <link rel="stylesheet" href="jquery/jquery-ui.css">
-    <link rel="stylesheet" href="jquery/jquery-ui.structure.css">
-    <link rel="stylesheet" href="jquery/jquery-ui.theme.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/StyleMenuAdmin.css">
 
-    <style>
-    @keyframes rellenar{
-        to{
-            stroke-dasharray: var(--porcentaje) 100;
-        }
-    }
-
-    .porcentajes{
-        position:relative;
-    }
-    .porcentajes span{
-        position: absolute;
-        top: 0%;
-        left: 0%;
-        bottom: 0%;
-        right: 0%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font: 25px/1em Verdana;
-    }
-
-    circle{
-        fill: none;
-        stroke-width: 20;
-        transform: rotate(-90deg);
-        transform-origin: 50%;
-        stroke-dasharray: 100 100;
-        stroke: #AAA;
-    }
-    circle:nth-child(2){ /* el segundo círculo, es el que se ve por encima del anterior y debe tener el color mas intenso y el area del porcentaje */
-        stroke: var(--color);
-        stroke-dasharray: 0 100;
-        animation: rellenar .35s linear forwards;
-    }
-  </style>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    
 </head>
-<body>
-    <button onclick="window.location.href = 'registro_admin.html'">Registrar personal</button>
-    <button onclick="window.location.href='registro_alumno.html'">Registrar alumno</button><br><br>
-    
-    <form method="GET">
-        <select name="generacion">
-        <option value="" disabled selected>Generación</option> 
-        <!-- Sirve para mostrar las generaciones que están registradas en la BD -->
-        <?php
-            $generacion = "SELECT * FROM generaciones";
-            $result = $conn->query($generacion);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<option value='" . $row['generacion'] . "'>" . $row['generacion'] . "</option>";
+<body background="Visual/Fondos/fondoMenuAdmin.jpg">
+
+    <div class="barra-superior">
+        <span>Administrador</span>
+        <a href="conexiones/cerrar_sesion.php">Cerrar sesión</a>
+    </div>
+
+    <div class="botones">
+        <button onclick="window.location.href = 'registro_admin.html'">Registrar personal</button>
+        <button onclick="window.location.href='registro_alumno.html'">Registrar alumno</button>
+    </div>
+
+    <div class="selector-generacion">
+        <form method="GET">
+            <select name="generacion">
+                <option value="" disabled selected>Generación</option>
+                <?php
+                // Conexión a la BD
+                $generacion = "SELECT * FROM generaciones";
+                $result = $conn->query($generacion);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row['generacion'] . "'>" . $row['generacion'] . "</option>";
+                    }
+                } else {
+                    echo "<option value=''>Generación no disponible</option>";
                 }
-            } 
-            else {
-                echo "<option value=''>Generación no disponible</option>";
-            }
-        ?>
-        </select>
-        <button type="submit">Mostrar</button>
-    </form>
-    
-    <!-- PHP para cambiar los datos de acuerdo a la generación para futuros registros  -->
-    <?php
+                ?>
+            </select>
+            <button type="submit">Mostrar</button>
+        </form>
+    </div>
+
+    <br>
+        <!--Progreso -->
+    <div class="contenedor-progreso">
+        <?php
         if (isset($_GET['generacion'])) {
+
             $generacion = $_GET['generacion'];
             
             $promedio_gen = "SELECT grupos.nombre AS grupo, 
@@ -97,6 +74,7 @@ include'conexiones/conexion.php';
             $general->execute();
             $pro_gen = $general->get_result();
             
+            
             if ($pro_gen->num_rows > 0) {
                 while ($row = $pro_gen->fetch_assoc()) {
                     $p_general = round($row['promedio_g'], 1);
@@ -111,21 +89,23 @@ include'conexiones/conexion.php';
                         $color = 'blue';
                     }
 
-                    // echo "<span><strong>Grupo " . $row['grupo'] . "</strong></span><br>
-                    // <span><strong>Promedio general: </strong>" . $p_general . "</span><br>
-                    // <span><strong>Promedio números: </strong>" . $p_numeros . "</span><br>
-                    // <span><strong>Promedio letras: </strong>" . $p_letras . "</span><br><br>";
+                    echo '<div class="container text-center"> ';
+                    echo "<span><strong>Grupo: " . $row['grupo'] . "</strong></span><br>";
 
-                    echo "<span><strong>Grupo " . $row['grupo'] . "</strong></span><br>";
+                    echo'
 
-                    echo
-                    '<span><strong>Promedio general: </strong>'. $p_general . '</span><br>
-                    <div class="porcentajes" style="--porcentaje: '.number_format($p_general, 1).'; --color: '.$color.'">
-                        <svg width="150" height="150">
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                        </svg>
-                    </div>';
+                    
+                        <div class="row">
+                            <div class="col">
+                                <span><strong>Promedio general: </strong>'. $p_general . '</span><br>
+                            <div class="porcentajes" style="--porcentaje: '.number_format($p_general, 1).'; --color: '.$color.'">
+                                <svg width="150" height="150">
+                                    <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                                    <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                                </svg>
+                            </div>
+                        </div>
+                    ';
 
                     if ($p_numeros < 6) {
                         $color = 'red';
@@ -136,13 +116,17 @@ include'conexiones/conexion.php';
                     }
 
                     echo
-                    '<span><strong>Promedio números: </strong>'. $p_numeros .'</span><br>
-                    <div class="porcentajes" style="--porcentaje: '.number_format($p_numeros, 1).'; --color: '.$color.'">
-                        <svg width="150" height="150">
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                        </svg>
-                    </div>';
+                    '
+                    <div class="col">
+                            <span><strong>Promedio números: </strong>'. $p_numeros .'</span><br>
+                        <div class="porcentajes" style="--porcentaje: '.number_format($p_numeros, 1).'; --color: '.$color.'">
+                            <svg width="150" height="150">
+                                <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                                <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                            </svg>
+                    </div>
+                        </div>
+                    ';
 
                     if ($p_letras < 6) {
                         $color = 'red';
@@ -153,13 +137,18 @@ include'conexiones/conexion.php';
                     }
 
                     echo
-                    '<span><strong>Promedio letras: </strong> '. $p_letras .' </span><br><br>
-                    <div class="porcentajes" style="--porcentaje: '.number_format($p_letras, 1).'; --color: '.$color.'">
-                        <svg width="150" height="150">
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                            <circle r="65" cx="50%" cy="50%" pathlength="10" />
-                        </svg>
-                    </div>';
+                    '
+                     <div class="col">
+                        <span><strong>Promedio letras: </strong> '. $p_letras .' </span><br><br>
+                        <div class="porcentajes" style="--porcentaje: '.number_format($p_letras, 1).'; --color: '.$color.'">
+                            <svg width="150" height="150">
+                                <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                                <circle r="65" cx="50%" cy="50%" pathlength="10" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                    ';
                 }
             } 
             else {
@@ -171,9 +160,9 @@ include'conexiones/conexion.php';
         else {
             echo "Selecciona una generación para ver los resultados.";
         }
-    ?>
 
-    <br><br>
-    <a href="conexiones/cerrar_sesion.php">Cerrar sesión</a>
+        ?>
+    </div>
+
 </body>
 </html>
