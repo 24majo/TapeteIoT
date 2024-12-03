199021d403ddd709fdc862cf5d4c244270f6641f
24majo <157389928+24majo@users.noreply.github.com>
2024-12-02 21:35:30 -0600

    mensajes sesión admin

--- inicioAdmin.php
+++ inicioAdmin.php
@@ -1,50 +1,49 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="./Tapete/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./Tapete/css/styleIndex.css">
-    
-    <script src="./Tapete/js/bootstrap.min.js"></script>
-    <script src="./Tapete/js/jquery-3.7.1.min.js"></script>
-    <title>Inicio sesión administrativos</title>
+  <link rel="stylesheet" href="./Tapete/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./Tapete/css/styleIndex.css">
+  
+  <script src="./Tapete/js/bootstrap.min.js"></script>
+  <script src="./Tapete/js/jquery-3.7.1.min.js"></script>
+  <title>Inicio sesión administrativos</title>
 </head>
 
 <script>
-      function Cancelar_d(){
-        document.getElementById("num_empleado").value = ""
-        document.getElementById("pass").value = ""
-      }
+  function Cancelar_d(){
+    document.getElementById("num_empleado").value = ""
+    document.getElementById("pass").value = ""
+  }
 
-      function Pregunta_d(event){
-        var num_e = $('#num_empleado').val();
-        if(num_e.trim() !== ""){
-          $.ajax({
-            url:'Tapete/conexiones/ingreso_admin.php',
-            type:'GET',
-            data: {num_e: num_e},
-            success: function(response){
-              if(response){
-                $('#pregunta_d').val(response);
-              }
-              else{
-                alert('Este empleado no tiene pregunta de seguridad');
-              }
-            },
-            error:function(){
-              alert('Error de consulta');
-            }
-          })
+  function Pregunta_d(event){
+    var num_e = $('#num_empleado').val();
+    if(num_e.trim() !== ""){
+      $.ajax({
+        url:'Tapete/conexiones/ingreso_admin.php',
+        type:'GET',
+        data: {num_e: num_e},
+        success: function(response){
+          if(response){
+            $('#pregunta_d').val(response);
+          }
+          else{
+            alert('Este empleado no tiene pregunta de seguridad');
+          }
+        },
+        error:function(){
+          alert('Error de consulta');
         }
-        event.preventDefault();
-      }
-    </script>
+      })
+    }
+    event.preventDefault();
+  }
+</script>
 <body  background="./Tapete/Visual/Fondos/FondoInicioSesion2.jpg">
-
-<div class="container2">
-<form class="formulario2" method="post" action="Tapete/conexiones/ingreso_admin.php">
+  <div class="container2">
+    <form class="formulario2" method="post">
       <h2>Ingreso de administrativos</h2>
       <label>Número de empleado</label>
       <br>
@@ -69,8 +68,142 @@
           <button id="verificar_d" name="verificar_d">Verificar</button>
         </center>
       </div>
-  </form>
-</div>
+    </form>
+  </div>
 
+  <?php
+    include'Tapete/conexiones/conexion.php';
+    session_start();
+    $pregunta ="";
+
+    if (isset($_POST['ingresar_d'])) {
+        $num_empleado = $_POST['num_empleado'];
+        $pass = $_POST['pass'];
+    
+        $sql = "SELECT password FROM docentes WHERE num_empleado = '$num_empleado'";
+        $rol = "SELECT rol FROM docentes where num_empleado = '$num_empleado'";
+        $result = $conn->query($sql);
+        $res_rol = $conn->query($rol);
+    
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $contra = $row['password'];
+    
+            if (password_verify($pass, $contra)) {
+
+                if($res_rol -> num_rows > 0){
+                    $roles = $res_rol->fetch_assoc();
+                    $rolF = $roles['rol'];
+                    //echo $rolF;
+                    if($rolF == 'admin'){
+                        $_SESSION['num_empleado'] = $num_empleado;
+                        header("Location: Tapete/MenuAdmin.php");
+                    }
+                    else if ($rolF == 'docente'){
+                        $_SESSION['num_empleado'] = $num_empleado;
+                        header("Location: Tapete/MenuDocentes.php");
+                    }
+                } 
+
+                else {
+                    echo '<script src="Tapete/node_modules/sweetalert/dist/sweetalert.min.js"></script>';
+                    echo 
+                    '<script>
+                        swal({
+                            title: "Rol no asignado",
+                            text: "Verifica que el usuario ingresado trnga asignado un rol de privilegios.",
+                            icon: "info"
+                        })
+                    </script>';
+                }
+            }
+                
+            
+            else {
+                echo '<script src="Tapete/node_modules/sweetalert/dist/sweetalert.min.js"></script>';
+                echo 
+                '<script>
+                    swal({
+                        title: "Contraseña incorrecta",
+                        text: "Verifica que tu contraseña sea correcta.",
+                        icon: "error"
+                    })
+                </script>';
+            }
+        } 
+        
+        else {
+            echo '<script src="Tapete/node_modules/sweetalert/dist/sweetalert.min.js"></script>';
+            echo 
+            '<script>
+                swal({
+                    title: "Usuario incorrecto",
+                    text: "Verifica que el número de empleado sea correcto o esté registrado en el sistema.",
+                    icon: "error"
+                })
+            </script>';
+        }
+    } 
+
+    if (isset($_GET['num_e'])) {
+        $num_e = $_GET['num_e'];
+        $sql = "SELECT pregunta FROM docentes WHERE num_empleado = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $num_e);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()) {
+            $pregunta = $row['pregunta'];
+            echo $pregunta;
+        } 
+        else {
+            $pregunta = 'Pregunta no encontrada';
+        }
+        exit;
+    }
+
+    if(isset($_POST["verificar_d"])){
+        $num_e = $_POST['num_empleado'];
+        $respuesta = $_POST['respuesta_d'];
+        $sql = "SELECT respuesta FROM docentes WHERE num_empleado = '$num_e'";
+        $rol = "SELECT rol FROM docentes where num_empleado = '$num_e'";
+        $result = $conn->query($sql);
+        $res_rol = $conn->query($rol);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $res = $row['respuesta'];
+    
+            if (password_verify($respuesta, $res)) {
+                $_SESSION['num_empleado'] = $num_e;
+                if($res_rol -> num_rows > 0){
+                    $roles = $res_rol->fetch_assoc();
+                    $rolF = $roles['rol'];
+                    if($rolF == 'admin'){
+                        $_SESSION['num_empleado'] = $num_e;
+                        header("Location: Tapete/MenuAdmin.php");
+                    }
+                    if ($rolF == 'docente'){
+                        $_SESSION['num_empleado'] = $num_e;
+                        header("Location: Tapete/MenuDocentes.php");
+                    }
+                } 
+                exit;
+            } 
+            else {
+                echo '<script src="Tapete/node_modules/sweetalert/dist/sweetalert.min.js"></script>';
+                echo 
+                '<script>
+                    swal({
+                        title: "Respuesta incorrecta",
+                        text: "Verifica que la respuesta a la pregunta sea correcta.",
+                        icon: "error"
+                    })
+                </script>';
+            }
+        }
+    }
+?>
 </body>
 </html>
