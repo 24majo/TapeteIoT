a74b53741b82a8433d4d47c6544f0580a6343318
24majo <157389928+24majo@users.noreply.github.com>
2024-12-05 13:54:21 -0600

    ya ta

--- Tapete/RegistroGrupo.php
+++ Tapete/RegistroGrupo.php
@@ -48,6 +48,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Visual/Material/Didit.png" type="image/x-icon">
     <title>Grupos</title>
 </head>
 <body>
