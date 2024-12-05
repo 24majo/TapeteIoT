f413fd0133b0384b647ba6aecb53857307c52033
24majo <157389928+24majo@users.noreply.github.com>
2024-12-05 13:53:38 -0600

    tab

--- Tapete/conexiones/preguntas.php
+++ Tapete/conexiones/preguntas.php
@@ -3,40 +3,40 @@ include'conexion.php';
 session_start();
 $pregunta = "";
 if (isset($_GET['curp'])) {
-    $curp = $_GET['curp'];
+  $curp = $_GET['curp'];
 
-    $sql = "SELECT Pregunta FROM usuarios WHERE CURP = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $curp);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  $sql = "SELECT Pregunta FROM usuarios WHERE CURP = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("s", $curp);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    if ($row = $result->fetch_assoc()) {
-        $pregunta = $row['Pregunta']; 
-        echo $pregunta;
-        exit;
-    } 
-    else {
-        $pregunta = 'Pregunta no encontrada';
-    }
+  if ($row = $result->fetch_assoc()) {
+    $pregunta = $row['Pregunta']; 
+    echo $pregunta;
     exit;
+  } 
+  else {
+    $pregunta = 'Pregunta no encontrada';
+  }
+  exit;
 }
 
 if (isset($_GET['num_e'])) {
-    $num_e = $_GET['num_e'];
-    $sql = "SELECT pregunta FROM docentes WHERE num_empleado = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $num_e);
-    $stmt->execute();
-    $result = $stmt->get_result();
+  $num_e = $_GET['num_e'];
+  $sql = "SELECT pregunta FROM docentes WHERE num_empleado = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("s", $num_e);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    if ($row = $result->fetch_assoc()) {
-      $pregunta = $row['pregunta'];
-      echo $pregunta;
-    } 
-    else {
-      $pregunta = 'Pregunta no encontrada';
-    }
-    exit;
+  if ($row = $result->fetch_assoc()) {
+    $pregunta = $row['pregunta'];
+    echo $pregunta;
+  } 
+  else {
+    $pregunta = 'Pregunta no encontrada';
   }
+  exit;
+}
 ?>
