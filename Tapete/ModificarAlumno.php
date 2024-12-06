0fb20feca24bb246f7c7b121447beebe55e8ba2e
24majo <157389928+24majo@users.noreply.github.com>
2024-12-06 10:44:39 -0600

    formulario simple

--- Tapete/ModificarAlumno.php
+++ Tapete/ModificarAlumno.php
@@ -1,3 +1,8 @@
+<?php
+    include'conexiones/conexion.php';
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,14 @@
     <title>Modificar Alumno</title>
 </head>
 <body>
+    <form action="" method="get">
+        <span>CURP</span><br>
+        <input type="text" name="curp" id="curp" disabled><br><br>
+        <span>Nombre</span><br>
+        <input type="text" name="nombre" id="nombre" disabled><br><br>
+        <span>Apellidos</span><br>
+        <input type="text" name="apellidos" id="apellidos" disabled><br><br>
+    </form>
     
 </body>
 </html>
